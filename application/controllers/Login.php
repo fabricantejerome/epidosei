ca90319ef6445149eeeb19f319883a2f6738646f
jfabricante123 <jerome.fabricante@opsolutions.biz>
2020-03-17 23:01:03 +0800

    Template Config

---
+++ application/controllers/Login.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		// $this->load->model('user_model');
+		$this->load->helper('form');
+		$this->twig->addGlobal('session', $this->session);
+	}
+
+	public function index()
+	{
+		$this->twig->display('login');
+    }
+    
+}
